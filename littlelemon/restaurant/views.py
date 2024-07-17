26bc8963e43187618cc62b4fc471f2e6e0e28d52
Mustafa Dogan <mdogan89@hotmail.com>
2024-07-17 19:58:03 +0300

    added booking and menu views

--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
 
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import Booking, MenuItem
+from .serializers import BookingSerializer, MenuItemSerializer
+from rest_framework.generics import (
+    ListCreateAPIView,
+    RetrieveUpdateAPIView,
+    DestroyAPIView,
+)
+
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAuthenticated
+
 # from django.http import HttpResponse
 
 # Create your views here.
@@ -7,3 +20,47 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, "index.html", {})
+
+
+# class BookingView(APIView):
+#     def get(self, request):
+#         items = Booking.objects.all()
+#         serializer = BookingSerializer(items, many=True)
+#         return Response(serializer.data)
+
+#     def post(self, request):
+#         serializer = BookingSerializer(data=request.data)
+
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response({"status": "success", "data": serializer.data})
+
+
+# class MenuView(APIView):
+#     def get(self, request):
+#         items = Menu.objects.all()
+#         serializer = MenuSerializer(items, many=True)
+#         return Response(serializer.data)
+
+#     def post(self, request):
+#         serializer = MenuSerializer(data=request.data)
+
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response({"status": "success", "data": serializer.data})
+
+
+class MenuItemView(ListCreateAPIView):
+    queryset = MenuItem.objects.all()
+    serializer_class = MenuItemSerializer
+
+
+class SingleMenuItemView(RetrieveUpdateAPIView, DestroyAPIView):
+    queryset = MenuItem.objects.all()
+    serializer_class = MenuItemSerializer
+
+
+class BookingViewSet(ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
+    permission_classes = [IsAuthenticated]

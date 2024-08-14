bcad0aa3c1df165c8f95f7b0f497165a49a7a950
Mustafa Dogan <mdogan89@hotmail.com>
2024-08-14 21:41:43 +0300

    installed whitenoise

--- littlelemon/littlelemon/settings.py
+++ littlelemon/littlelemon/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware"
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -150,3 +151,5 @@ REST_FRAMEWORK = {
 
 
 DJOSER = {"USER_ID_FIELD": "username"}
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"

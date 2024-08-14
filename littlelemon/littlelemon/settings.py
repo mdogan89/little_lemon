ff0f0521cfebe7b47ff95e6c708f5089c05ba866
Mustafa Dogan <mdogan89@hotmail.com>
2024-08-14 22:50:47 +0300

    changed static folder

--- littlelemon/littlelemon/settings.py
+++ littlelemon/littlelemon/settings.py
@@ -47,8 +47,8 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
-    "whitenoise.middleware.WhiteNoiseMiddleware"
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
@@ -125,8 +125,8 @@ USE_TZ = True
 STATIC_URL = "static/"
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-    os.path.join(BASE_DIR, "/restaurant/static"),
+    # os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, "restaurant/static"),
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")

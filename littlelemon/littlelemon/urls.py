d371e7cf845b48500fc56ccd5682e4f989e328dd
Mustafa Dogan <mdogan89@hotmail.com>
2024-08-14 22:38:39 +0300

    added static url

--- littlelemon/littlelemon/urls.py
+++ littlelemon/littlelemon/urls.py
@@ -24,6 +24,9 @@ from rest_framework.authtoken.views import obtain_auth_token
 router = routers.DefaultRouter()
 router.register(r"tables", BookingViewSet)
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("restaurant/", include("restaurant.urls")),
@@ -32,3 +35,5 @@ urlpatterns = [
     path("auth/", include("djoser.urls.authtoken")),
     path("api-token-auth/", obtain_auth_token),
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)

5e802f27f62f98e93cfbedac30b8355ada4e4feb
Mustafa Dogan <mdogan89@hotmail.com>
2024-08-14 21:28:04 +0300

    changed settings.py

--- littlelemon/littlelemon/settings.py
+++ littlelemon/littlelemon/settings.py
@@ -125,10 +125,10 @@ STATIC_URL = "static/"
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
-    os.path.join(BASE_DIR, "restaurant/static"),
+    # os.path.join(BASE_DIR, "restaurant/static"),
 ]
 
-STATIC_ROOT = "/staticfiles/"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field

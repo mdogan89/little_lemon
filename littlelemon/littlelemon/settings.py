8c245556d6924dd13914eb4e613d07e58d7bced9
Mustafa Dogan <mdogan89@hotmail.com>
2024-08-14 21:30:41 +0300

    change settings.py

--- littlelemon/littlelemon/settings.py
+++ littlelemon/littlelemon/settings.py
@@ -125,7 +125,7 @@ STATIC_URL = "static/"
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
-    # os.path.join(BASE_DIR, "restaurant/static"),
+    os.path.join(BASE_DIR, "/restaurant/static"),
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
